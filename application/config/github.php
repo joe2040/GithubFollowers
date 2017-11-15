02317914baef8004f2e1d4a19625d69c2687eb97
cenveo16 <cenveo16@gmail.com>
2017-11-15 21:04:09 +0530

    change in config--github

--- application/config/github.php
+++ application/config/github.php
@@ -1,6 +1,12 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * [$config description]
- * @var [type]
- */
-$config['access_token'] = 'e555ed7b0250a8a24526e7ecafce638c9e0eba80';
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+
+ * [$config description]
+
+ * @var [type]
+
+ */
+
+$config['access_token'] = 'Your Personal access token';
+
